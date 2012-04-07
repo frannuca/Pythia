4160974353226d99a09a2245670a15a52edfb95f
fran <frannuca@gmail.com>
2012-04-07 10:12:31 +0200

    removing plugins

--- project/Build.scala
+++ project/Build.scala
@@ -61,7 +61,19 @@ object Resolvers {
   val metro = Resolver.url("Metro Web Services",url("https://metro.dev.java.net/"))
 
 
+  resolvers += "retronym-releases" at "http://retronym.github.com/repo/releases"
 
+  resolvers += "retronym-snapshots" at "http://retronym.github.com/repo/snapshots"
+
+  resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+
+  resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
+
+  resolvers += "sbt-idea-repo" at "http://mpeltonen.github.com/maven/"
+
+  addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.0.0")
+
+  addSbtPlugin("com.github.retronym" % "sbt-onejar" % "0.7")
 
 }
 
